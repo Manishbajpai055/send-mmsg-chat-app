0b57a641922bd407c4b7902b89528e75cf3d483d
manishbajpai055 <manishbajpai055@gmail.com>
2021-01-30 20:21:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+const path = require('path');
+const express = require('express');
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on('connection', (socket) => {
+  console.log("connected 1 user")
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+    
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
